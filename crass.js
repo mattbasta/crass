11958b381f1989948ddef213aa25e96be2436cab
Matt Basta <mattbasta+github@gmail.com>
2014-09-22 17:17:15 -0700

    Expose crass objects

--- crass.js
+++ crass.js
@@ -1,7 +1,11 @@
+var objects = require('./lib/objects');
+
 module.exports.parse = function(data) {
     var p = require('./grammar');
     var parser = new p.Parser();
-    parser.yy = require('./lib/objects');
+    parser.yy = objects;
 
     return parser.parse(data + '');
 };
+
+module.exports.objects = objects;

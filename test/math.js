a6b91d8cb206c4aaa0a6e5d196048dd5cf927ee4
Matt Basta <mbasta@box.com>
2015-03-03 15:01:46 -0800

    Fixes #24
    Remove optional padding on MathSum node's toString method

--- test/math.js
+++ test/math.js
@@ -8,16 +8,16 @@ var parseString = function(data) {
 
 describe('Math Expressions', function() {
     it('should parse', function() {
-        assert.equal(parseString('a{foo:calc(50% - 100px)}'), 'a{foo:calc(50%-100px)}');
+        assert.equal(parseString('a{foo:calc(50% - 100px)}'), 'a{foo:calc(50% - 100px)}');
     });
     it('should parse with products', function() {
         assert.equal(parseString('a{foo:calc(50% * 100px)}'), 'a{foo:calc(50%*100px)}');
         assert.equal(parseString('a{foo:calc(50% / 100px)}'), 'a{foo:calc(50%/100px)}');
-        assert.equal(parseString('a{foo:calc(5px + 50% * 100px)}'), 'a{foo:calc(5px+50%*100px)}');
+        assert.equal(parseString('a{foo:calc(5px + 50% * 100px)}'), 'a{foo:calc(5px + 50%*100px)}');
     });
     it('should parse with sums in products', function() {
-        assert.equal(parseString('a{foo:calc((5px + 50%) * 100px)}'), 'a{foo:calc((5px+50%)*100px)}');
-        assert.equal(parseString('a{foo:calc(100px * (5px + 50%))}'), 'a{foo:calc(100px*(5px+50%))}');
+        assert.equal(parseString('a{foo:calc((5px + 50%) * 100px)}'), 'a{foo:calc((5px + 50%)*100px)}');
+        assert.equal(parseString('a{foo:calc(100px * (5px + 50%))}'), 'a{foo:calc(100px*(5px + 50%))}');
     });
     it('should pretty print', function() {
         assert.equal(
@@ -38,7 +38,11 @@ describe('Math Expressions', function() {
     it('should optimize the terms of a sum', function() {
         assert.equal(
             crass.parse('a{foo:calc(12pt + 96px)}').optimize().toString(),
-            'a{foo:calc(1pc+1in)}'
+            'a{foo:calc(1pc + 1in)}'
+        );
+        assert.equal(
+            crass.parse('a{foo:calc(12pt - 96px)}').optimize().toString(),
+            'a{foo:calc(1pc - 1in)}'
         );
     });
 });
